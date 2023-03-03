fbfce65bbb0846ce1ca926aba1011fea4beee7d5
DavidAshburn <oneinchwebbing@gmail.com>
2023-03-03 12:11:29 -0800

    tailwind config and google fonts

---
+++ config/tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './public/*.html',
+    './app/helpers/**/*.rb',
+    './app/javascript/**/*.js',
+    './app/views/**/*.{erb,haml,html,slim}'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        'abril': ['Abril Fatface', 'cursive'],
+        'garamond': ['Cormorant Garamond', 'serif'],
+        'inter': ['Inter Tight', 'sans-serif'],
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'nunito': ['Nunito', 'sans-serif'],
+        'playfair': ['Playfair Display', 'serif'],
+        'tinos': ['Tinos', 'serif'],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/typography'),
+  ]
+}
