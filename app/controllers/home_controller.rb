05b0dbf64e03f4e9044d874d3c9e5a500635e264
DavidAshburn <oneinchwebbing@gmail.com>
2023-03-04 21:29:56 -0800

    page structure

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,16 @@
 class HomeController < ApplicationController
   def index
   end
+  def poetry
+  end
+  def art
+  end
+  def issues
+  end
+  def submit
+  end
+  def support
+  end
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   root 'home#index'
   get 'home/fonts'
+  get 'home/poetry'
+  get 'home/art'
+  get 'home/issues'
+  get 'home/submit'
+  get 'home/support'
+  get 'home/about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
