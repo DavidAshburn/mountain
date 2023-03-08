2b87a3a2ef56a48cf5a83ef2d6a916b2c55fa22d
DavidAshburn <oneinchwebbing@gmail.com>
2023-03-07 22:56:57 -0800

    4 poets added

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -1,4 +1,20 @@
 class PoetsController < ApplicationController
   def bgreen
   end
+  def ldelan
+  end
+  def ewij
+  end
+  def mbrooks
+  end
+  def wrmoore
+  end
+  def lbridges
+  end
+  def dradavich
+  end
+  def kljohnston
+  end
+  def thbodine
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,10 @@ Rails.application.routes.draw do
   get 'poets/ewij'
   get 'poets/mbrooks'
   get 'poets/wrmoore'
+  get 'poets/lbridges'
+  get 'poets/dradavich'
+  get 'poets/kljohnston'
+  get 'poets/thbodine'
 
   mount PdfjsViewer::Rails::Engine => "/pdfjs", as: 'pdfjs'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
